abbf886f2d2aaea4bdffc4d5d8520700b8506810
JunSeong Bae <gentlmanjun@naver.com>
2021-10-08 11:20:54 +0900

    to do list 창 디자인 개선

--- todo.js
+++ todo.js
@@ -22,7 +22,7 @@ function paintToDo(toDoInputValue) {
   li.id = toDoInputValue.id;
   const span = document.createElement("span");
   const button = document.createElement("button");
-  button.innerText = "❌";
+  button.innerText = "X";
   button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
